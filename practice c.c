c2987d2ffb1f53f4dfc8b089770417d012c63948
Gazi Akter <gaziakterpro@gmail.com>
2022-10-24 07:54:38 +0600

    C data type

--- practice c.c
+++ practice c.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int main(){
-int num1 = 10, num2 = 12;
-printf("Number are %d and %d", num1, num2);
+float mum1 = 10.5;
+double num2 = 10.5255666666;
+char ch = 'a';
+
+printf("This is float DataType %f\n", mum1);
+printf("This is double DataType %lf\n", num2);
+printf("This is char %c",ch);
 
 getch();
 }
