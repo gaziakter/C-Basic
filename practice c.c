65d39cf3fab7cb3fefe74cc12e522e3bffbe17ea
Gazi Akter <gaziakterpro@gmail.com>
2022-10-26 06:49:30 +0600

    printf and scanf funtion use

--- practice c.c
+++ practice c.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-float mum1 = 10.5;
-double num2 = 10.5255666666;
-char ch = 'a';
 
-printf("This is float DataType %f\n", mum1);
-printf("This is double DataType %lf\n", num2);
-printf("This is char %c",ch);
+int yournum;
+
+printf("Please enter an integer:");
+scanf("%d", &yournum);
+printf("You entered: %d\n", yournum);
+
 
 getch();
 }
